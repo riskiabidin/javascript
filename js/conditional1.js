b5f0f629752e77bb094687dbf8d89c71b6b80871
riski87654321 <riskiaktor@gmail.com>
2020-10-19 07:15:32 +0700

    conditional1

---
+++ js/conditional1.js
@@ -0,0 +1,8 @@
+//penggunaan satu if digunakan jika kondisi if terpenuhi maka akan mengeksekusi blok code {}
+// prompt (digunakan untuk menampung/menginputkan suatu data didalam javascript).
+var totalBelanja=prompt("total belanja ?", 0);
+
+if(totalBelanja > 30000){
+    document.write("<h2>selamat anda mendapatkan sandal</h2>");
+}
+document.write("<p>terimakasih sudah berbelanja diwarung kami</p>");
---
+++ js/vif1.js
@@ -0,0 +1,8 @@
+var password=prompt("password:");
+if(password=="belajarjs"){
+    document.write("<h2>selamat anda berhasil login");
+}else{
+    document.write("<h2>maaf, password yang anda masukan salah, silahkan ulangi lagi");
+}
+
+document.write("<p>terimakasih sudah menggunakan aplikasi ini bro</p>");
